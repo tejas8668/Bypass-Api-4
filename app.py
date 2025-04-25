dadb48429d3f1513aa53f2c7f197bbb07216a085
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2025-04-25 18:02:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import time
 import cloudscraper
 import cfscrape
 from bs4 import BeautifulSoup
+import os
 
 app = Flask(__name__)
 
@@ -218,5 +219,9 @@ def decode_seturl():
 
     return jsonify({"final_url": final_link})
 
+#if __name__ == '__main__':
+    #app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
